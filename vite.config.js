6c43901bd382e340c1dec4254d3dddf77b2a0de6
jefailhamprayoga13 <jefailhamprayoga13@gmail.com>
2023-03-30 01:21:32 +0700

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000", // Ganti dengan URL Fake API JSON Server yang sedang berjalan di komputer lokal Anda
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
+})
